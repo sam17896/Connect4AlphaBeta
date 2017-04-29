2eb2382e530f78b81a7dbe4db5f96b4fae7c26f3
Ahsan Sohail <k142109@nu.edu.pk>
2017-04-30 03:11:19 +0500

    clean up board class

--- src/cs401k142109a2/Board.java
+++ src/cs401k142109a2/Board.java
@@ -5,14 +5,11 @@ import java.util.List;
 class Board {
     public static final int ROWS = 4;
     public static final int COLS = 6;
-    //board[row][col]
+    
     private int[][] board;
     
-    //gives the first empty row in each column.
-    //very useful.
     protected int[] indices;
 
-    //rows, columns, and diagonals as strings
     protected List<String> rows;
     protected List<String> cols;
     protected List<String> diagonals;
@@ -20,7 +17,7 @@ class Board {
     String[][] diagonalPosition;
 
     public Board() {
-        //board[row][col]
+    
         board = new int[ROWS][COLS];
         diagonalPosition =  new String[ROWS][COLS];
         indices = new int[6];
@@ -30,63 +27,36 @@ class Board {
         cols = new ArrayList();
         diagonals = new ArrayList();
                 
-        refreshRCD();
+        setRCD();
     }
     
-    public void undoMove(int player_piece, int move_position) {
-        //System.out.println("before undo\n" + this);
-        int row = indices[move_position];
+    public void undoMove(int turn, int column) {
+        
+        // turn = 1 for player
+        // turn = 2 for computer
+        
+        // column = move taken by player
+        
+        int row = indices[column];
         if (row == 0 || row > ROWS) {
-            throw new RuntimeException("Illegal undoMove("
-                    +player_piece + "," + move_position + ")");
+            System.out.println("\nError: Can't undo this move\n");
         }
-        indices[move_position]--;
-        board[row-1][move_position] = 0;
-        refreshRCD2(0, row-1, move_position);
-        //System.out.println("after undo\n" + this);
-    }
-/*
-    public Board(Board other) {
-        board = new int[ROWS][COLS];
-        indices = new int[6];
-
-        rows = new ArrayList();
-        cols = new ArrayList();
-        diagonals = new ArrayList();
-
-        for (int row = 0; row < ROWS; row++) {
-            System.arraycopy(other.board[row], 0, board[row], 0, COLS);
-        }
-        System.arraycopy(other.indices, 0, indices, 0, 6);
-
-        position_to_down_left_diagonal = new int[ROWS][COLS][2];
-        position_to_down_right_diagonal = new int[ROWS][COLS][2];
-
-        refreshRCD();
+        indices[column]--;
+        board[row-1][column] = 0;
+        updateRCD(0, row-1, column);
+ 
     }
 
-    public Board invert() {
-        Board other = new Board();
-        for (int row = 0; row < ROWS; row++) {
-            for (int col = 0; col < COLS; col++) {
-                if (board[row][col] == 1) {
-                    other.makeMove(2, col);
-                }
-                else if (board[row][col] == 2) {
-                    other.makeMove(1, col);
-                }
-            }
-        }
-        return other;
-    }
-*/
     public int[][] getBoardArray() {
         return board;
     }
+    
     public int pieceAt(int row, int col) {
         return board[row][col];
     }
 
+    
+    // to print the board
     @Override
     public String toString() {
         String Board = new String();
@@ -108,23 +78,25 @@ class Board {
         return Board;
     }
 
-    public void makeMove(int player_piece, int move_position) {
-        //get the empty row value in indices
-        int row = indices[move_position];
+    public void makeMove(int turn, int column) {
+        
+        // turn = 1 for player
+        // turn = 2 for computer
+        
+        // column = move taken by player
+       
+        int row = indices[column];
         if (row != ROWS) {
-            board[row][move_position] = player_piece;
-            //System.out.println(player_piece + " player piece");
-            indices[move_position]++;
+            board[row][column] = turn;
+            indices[turn]++;
                 
-            //now, correct the row/col/diagonal storage
-            refreshRCD2(player_piece, row, move_position);
+            updateRCD(turn, row, column);
             return;
         }
-        //if the program has not returned, we have an issue
-        throw new RuntimeException("Illegal makeMove(" + player_piece 
-                + "," + move_position + ")");
+        
+        System.out.println("\nError: Wrong Move\n");
     }
-    private void refreshRCD2(int piece, int row, int col) {
+    private void updateRCD(int piece, int row, int col) {
         char newPiece = (char)(piece+48);
 
         String r = rows.get(row);
@@ -157,7 +129,6 @@ class Board {
             }else{
                 int pos = Integer.parseInt(arg[1]);
                 int posX = Integer.parseInt(arg[2]);
-                //  System.out.println(pos + " " + posX + " yahan print ho raha hai andhe");
                 String d = diagonals.get(pos);
                 d = d.substring(0,posX) +  newPiece + d.substring(posX+1); 
                 diagonals.set(pos, d);
@@ -169,7 +140,7 @@ class Board {
 
     //return: 0 for no win, 1 for player 1 win, 2 for player 2 win
     public int isGameOver() {
-        //refreshRCD();
+    
         for (String s : rows) {
             if (s.contains("1111")) {
                 return 1;
@@ -200,7 +171,6 @@ class Board {
     public List<Integer> getPossibleMoves() {
         List<Integer> result = new ArrayList();
         for(int col = 0; col < COLS; col++) {
-            //check if any of the columns are not full
             if (indices[col] != ROWS) {
                 result.add(col);
             }
@@ -208,46 +178,14 @@ class Board {
         return result;
     }
 
-//    public List<Move> getPossibleMoves(int player) {
-//        List<Move> result = new ArrayList();
-//        for(int col = 0; col < COLS; col++) {
-//            //check if any of the columns are empty
-//            boolean isColumnFull = true;
-//            searchcol:
-//            for(int row = 0; row < ROWS; row++) {
-//                if (board[row][col] == 0) {
-//                    isColumnFull = false;
-//                    break searchcol;
-//                }
-//            }
-//            if (!isColumnFull) {
-//                result.add(new Move(player, col));
-//            }
-//        }
-//        return result;
-//    }
-
-    /*
-     * all of the rows, columns, and diagonals will be
-     * encoded in strings.
-     * this will then allow for the easy searching of
-     * substrings using the string api
-     *
-     */
-    //rows, columns, and diagonals
-//    protected List<String> getRCDasStr() {
-//        List<String> result = new ArrayList();
-//        result.addAll(getRowsAsStr());
-//        result.addAll(getColsAsStr());
-//        result.addAll(getDiagsAsStr());
-//        return result;
-//    }
-    protected void refreshRCD() {
+    protected void setRCD() {
+        
         for(int i=0;i<ROWS;i++){
             for(int j=0;j<COLS;j++){
                 diagonalPosition[i][j] = "";
             }
         }
+        
         rows = getRowsAsStr();
         cols = getColsAsStr();
         diagonals = getDiagsAsStr();
@@ -280,6 +218,7 @@ class Board {
     protected List<String> getDiagsAsStr()  {
         List<String> result = new ArrayList();
         int count = 0;
+        
         // right diagonals
         int row = 0;
         for(int i=0;i<3;i++){
@@ -322,14 +261,5 @@ class Board {
         }
     }
 
-//    //changes the position (row, column) to a corresponding
-//    //down-left diagonal position.
-//    public static final int[][][] left_diag_lookup =
-//        { { {9, 5}, {8, 5}, {7, 4}, {6, 3}, {-1, 0}, {-1,0}, {-1,0} },
-//          { {10, 4},{9, 4}, {8, 4}, {7, 3}, {6, 2}, {-1, 0}, {-1, 0}},
-//          { {11,3}, {10,3}, {9, 3}, {8, 3}, {7, 2}, {6, 1}, {-1, 0}},
-//          { {-1,0}, {11,2}, {10,2}, {9, 2}, {8, 2}, },
-//          {},
-//          {}};
 }
 
